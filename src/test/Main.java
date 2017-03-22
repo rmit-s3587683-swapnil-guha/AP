3c4e26328c9dd2828f2c4e7796218c19d1ca2925
rmit-s3587683-swapnil-guha <s3587683@student.rmit.edu.au>
2017-03-22 20:16:29 +1100

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author my
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
